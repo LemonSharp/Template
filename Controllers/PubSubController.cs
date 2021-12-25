9e697f71c02e30b02623886c3879a9f33c189db9
Weihan Li <weihanli@outlook.com>
2021-12-25 15:15:31 +0800

    init

---
+++ Controllers/PubSubController.cs
@@ -0,0 +1,31 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Dapr;
+using Dapr.Client;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DaprTest.Controllers;
+
+[Route("[controller]/[action]")]
+public class PubSubController: ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> Publish([FromServices] DaprClient daprClient)
+    {
+        await daprClient.PublishEventAsync("redis", "test", new
+        {
+            Message = "test"
+        });
+        return Ok();
+    }
+
+    [HttpPost]
+    [Topic("redis", "test")]
+    public IActionResult Consume(object msg)
+    {
+        Console.WriteLine("Consume triggered");
+        return Ok();
+    }
+}
---
+++ Controllers/ServiceInvocationController.cs
@@ -0,0 +1,26 @@
+﻿using Dapr.Client;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DaprTest.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class ServiceInvocationController: ControllerBase
+    {
+        [HttpGet]
+        [HttpPost]
+        public IActionResult Hello()
+        {
+            return Ok(new
+            {
+                Msg = "Hello world"
+            });
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Test([FromServices]DaprClient daprClient)
+        {
+            var result = await daprClient.InvokeMethodAsync<object>("dapr-test", "ServiceInvocation/Hello");
+            return Ok(new { Result = result });
+        }
+    }
+}
